b8a99105d4fbf9f60f75026f7a845467e414e4c3
Mariano R. <mariano_ramira@epam.com>
2026-01-19 16:25:59 -0300

    feat: rag tool

--- task/app.py
+++ task/app.py
@@ -50,12 +50,12 @@ class GeneralPurposeAgentApplication(ChatCompletion):
         tools.append(FileContentExtractionTool(endpoint=DIAL_ENDPOINT))
 
         # 4. Add RagTool with DIAL_ENDPOINT, DEPLOYMENT_NAME, and create DocumentCache (it has static method `create`)
-        # document_cache = DocumentCache.create() # max_items=10, ttl_seconds=3600)
-        # tools.append(RagTool(
-        #     endpoint=DIAL_ENDPOINT,
-        #     deployment_name=DEPLOYMENT_NAME,
-        #     document_cache=document_cache
-        # ))
+        document_cache = DocumentCache.create() # max_items=10, ttl_seconds=3600)
+        tools.append(RagTool(
+            endpoint=DIAL_ENDPOINT,
+            deployment_name=DEPLOYMENT_NAME,
+            document_cache=document_cache
+        ))
 
         # 5. Add PythonCodeInterpreterTool with DIAL_ENDPOINT, `http://localhost:8050/mcp` mcp_url, tool_name is
         #    `execute_code`, more detailed about tools see in repository https://github.com/khshanovskyi/mcp-python-code-interpreter
